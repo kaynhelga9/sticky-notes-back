88ecb50b3b593f23d462d698d060dd4c77d9b50e
Kayn Helga <32682968+kaynhelga9@users.noreply.github.com>
2022-12-03 04:03:19 +0700

    add port 3000 (frontend) to cors whitelist

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
 	"http://localhost:3500",
 	"https://www.stickynotes.com",
-	"https://stickynotes.com"
+	"https://stickynotes.com",
+	"http://localhost:3000",
 ];
 
 module.exports = allowedOrigins
